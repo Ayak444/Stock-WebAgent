fe162d414696a466eb6e360a2e826ef15c763a3e
nought2222-wei <nought2222@gmail.com>
2026-05-08 16:23:39 +0800

    Update news_crawler.py

--- news_crawler.py
+++ news_crawler.py
@@ -7,7 +7,7 @@ import requests
 from datetime import datetime
 from bs4 import BeautifulSoup
 import time
-
+#
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                   "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36",
